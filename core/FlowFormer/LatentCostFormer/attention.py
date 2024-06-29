d30e1aa4572265bd274bf5e2443fbd226d43c205
Yutian Chen <yutianch@andrew.cmu.edu>
2024-06-28 21:47:02 -0400

    Reduce bandwidth for position embedding

--- core/FlowFormer/LatentCostFormer/attention.py
+++ core/FlowFormer/LatentCostFormer/attention.py
@@ -88,49 +88,6 @@ class MultiHeadAttention(nn.Module):
 
         return out
 
-# class MultiHeadAttentionRelative_encoder(nn.Module):
-#     def __init__(self, dim, heads):
-#         super(MultiHeadAttentionRelative, self).__init__()
-#         self.dim = dim
-#         self.heads = heads
-#         self.scale = (dim/heads) ** -0.5
-#         self.attend = nn.Softmax(dim=-1)
-
-#     def attend_with_rpe(self, Q, K, Q_r, K_r):
-#         """
-#             Q: [BH1W1, H3W3, dim]
-#             K: [BH1W1, H3W3, dim]
-#             Q_r: [BH1W1, H3W3, H3W3, dim]
-#             K_r: [BH1W1, H3W3, H3W3, dim]
-#         """
-
-#         Q = rearrange(Q, 'b i (heads d) -> b heads i d', heads=self.heads) # [BH1W1, heads, H3W3, dim]
-#         K = rearrange(K, 'b j (heads d) -> b heads j d', heads=self.heads) # [BH1W1, heads, H3W3, dim]
-#         K_r = rearrange(K_r, 'b j (heads d) -> b heads j d', heads=self.heads) # [BH1W1, heads, H3W3, dim]
-#         Q_r = rearrange(Q_r, 'b j (heads d) -> b heads j d', heads=self.heads) # [BH1W1, heads, H3W3, dim]
-
-#         # context-context similarity
-#         c_c = einsum('bhid, bhjd -> bhij', Q, K) * self.scale # [(B H1W1) heads H3W3 H3W3]
-#         # context-position similarity
-#         c_p = einsum('bhid, bhjd -> bhij', Q, K_r) * self.scale # [(B H1W1) heads 1 H3W3]
-#         # position-context similarity
-#         p_c = einsum('bhijd, bhikd -> bhijk', Q_r[:,:,:,None,:], K[:,:,:,None,:])
-#         p_c = torch.squeeze(p_c, dim=4)
-#         p_c = p_c.permute(0, 1, 3, 2)
-#         dots = c_c + c_p + p_c
-#         return self.attend(dots)
-
-#     def forward(self, Q, K, V, Q_r, K_r):
-#         attn = self.attend_with_rpe(Q, K, Q_r, K_r)
-#         B, HW, _ = Q.shape
-
-#         V = rearrange(V, 'b j (heads d) -> b heads j d', heads=self.heads)
-
-#         out = einsum('bhij, bhjd -> bhid', attn, V)
-#         out = rearrange(out, 'b heads hw d -> b hw (heads d)', b=B, hw=HW)
-
-#         return out
-
 class MultiHeadAttentionRelative(nn.Module):
     def __init__(self, dim, heads):
         super(MultiHeadAttentionRelative, self).__init__()
@@ -178,7 +135,21 @@ def LinearPositionEmbeddingSine(x: torch.Tensor, dim: int = 128, NORMALIZE_FACOR
     # 200 should be enough for a 8x downsampled image
     # assume x to be [_, _, 2]
     freq_bands = torch.linspace(0, dim//4-1, dim//4, device=x.device)
-    return torch.cat([torch.sin(3.14*x[..., -2:-1]*freq_bands*NORMALIZE_FACOR), torch.cos(3.14*x[..., -2:-1]*freq_bands*NORMALIZE_FACOR), torch.sin(3.14*x[..., -1:]*freq_bands*NORMALIZE_FACOR), torch.cos(3.14*x[..., -1:]*freq_bands*NORMALIZE_FACOR)], dim=-1)
+    
+    width: int = freq_bands.size(0)
+    result = torch.empty((x.size(0), x.size(1), width * 4), device=x.device)
+    result[..., width * 0 : width * 1] = x[..., -2:-1] * freq_bands
+    result[..., width * 1 : width * 2] = x[..., -2:-1] * freq_bands
+    result[..., width * 2 : width * 3] = x[..., -1:]   * freq_bands
+    result[..., width * 3 : width * 4] = x[..., -1:]   * freq_bands
+    
+    result *= NORMALIZE_FACOR * torch.pi
+    
+    result[..., width * 0 : width * 1] = result[..., width * 0 : width * 1].sin_()
+    result[..., width * 1 : width * 2] = result[..., width * 1 : width * 2].cos_()
+    result[..., width * 2 : width * 3] = result[..., width * 2 : width * 3].sin_()
+    result[..., width * 3 : width * 4] = result[..., width * 3 : width * 4].cos_()
+    return result
 
 def ExpPositionEmbeddingSine(x: torch.Tensor, dim: int = 128, NORMALIZE_FACOR: float =1/200):
     # 200 should be enough for a 8x downsampled image
